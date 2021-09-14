9e7474110a9aa5958027ccfea1b3a16d1c03d98c
h3yon <khykhy1006@gmail.com>
2021-09-14 16:39:44 +0900

    webhook test1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 4000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("webhook well?");
+});
